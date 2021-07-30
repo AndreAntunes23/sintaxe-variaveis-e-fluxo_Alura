2e2a958cf8cfaf77760ad9bb1bca525bf55217c8
Andre <andre_lfa@outlook.com>
2021-07-30 16:21:30 -0300

    Class TestaEscopo

---
+++ TestaEscopo.java
@@ -0,0 +1,26 @@
+public class TestaEscopo {
+	public static void main(String[] args) {
+
+		System.out.println("Testando condicionais");
+		int idade = 20;
+		int QuantidadePessoa = 3;
+
+		// boolean acompanhado = QuantidadePessoa >= 2;
+		
+		boolean acompanhado;
+		
+		if (QuantidadePessoa >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+
+		System.out.println("valor de acompanhado " + acompanhado);
+		if (idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		} else {
+			System.out.println("infelizmente você não pode entrar");
+		}
+	}
+
+}
